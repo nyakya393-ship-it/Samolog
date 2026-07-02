b1b9e48f50d37d254f599508bfa9ce7bce240ba1
Sio <nyakya393@gmail.com>
2026-07-03 08:41:21 +0900

    Delete store.js

--- store.js
+++
@@ -1,9 +0,0 @@
-const STORAGE_KEY = "sr_v2";
-
-export function load(){
-  return JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
-}
-
-export function save(data){
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-}
