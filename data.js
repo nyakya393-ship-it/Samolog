fca2b59e80e0e7f0a7251d2a5c23ec4dbf365eb6
Sio <nyakya393@gmail.com>
2026-07-03 08:23:23 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,4 @@
+const weapons = ["わかば","スシ","ローラー","チャージャー","ブラスター","シェルター"];
+const specials = ["トルネード","ナイスダマ","ジェッパ","カニ","サメ","メガホン"];
+
+let data = JSON.parse(localStorage.getItem("sr") || "[]");
